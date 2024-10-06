28a6bf68fe00a0afa0d38e4a7ad95f79c3532488
Emmanuel Makinde <emmanueltofunmi9@gmail.com>
2024-10-06 14:10:50 +0100

    fast

---
+++ quiz/quiz.js
@@ -0,0 +1,24 @@
+function checkAnswer(event){
+    event.preventDefault();
+    const correctAnswer = '4';
+
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+    const feedbackDiv = document.getElementById('feedback');
+
+    if (!selectedAnswer) {
+        feedbackDiv.textContent = "Please select an answer.";
+        feedbackDiv.style.color ="red";
+        return;
+    }
+
+    const userAnswer = selectedAnswer.value;
+
+    if (userAnswer === correctAnswer) {
+        feedbackDiv.textContent = "Correct! Well done.";
+        feedbackDiv.style.color = "green";
+    } else {
+        feedbackDiv.textContent = "That's incorrect. Try again!";
+        feedbackDiv.style.color = 'blue';
+    }
+}
+document.getElementById("submit-answer").addEventListener('click', checkAnswer);
